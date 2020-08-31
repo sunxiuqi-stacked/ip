2aac7ab1bb098ec0b53a4311958a94ce5f00929a
sunxiuqi-stacked <59859941+sunxiuqi-stacked@users.noreply.github.com>
2020-08-31 19:07:05 +0800

    First push

---
+++ src/main/java/Task.java
@@ -0,0 +1,18 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+    protected int listCount;
+
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+        this.listCount = 0;
+    }
+
+    public int length() {
+        return listCount; //return tick or X symbols
+    }
+
+
+}
