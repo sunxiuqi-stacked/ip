d0c0974ce93b7b72140d6d09778677f7c5582240
sunxiuqi-stacked <59859941+sunxiuqi-stacked@users.noreply.github.com>
2020-08-31 19:02:11 +0800

    No changes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ public class Duke {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
+                + "| |_| | |_| \   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
     }
